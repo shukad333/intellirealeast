3675216cbb1cd14347a3b57530c7a4a0bd3c9141
shukad333@gmail.com <shuhail@Shuhails-MacBook-Air.local>
2024-02-01 00:08:41 +0530

    initial commit

---
+++ src/main/java/com/ai/realestate/intellirealeast/IntellirealeastApplication.java
@@ -0,0 +1,13 @@
+package com.ai.realestate.intellirealeast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntellirealeastApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntellirealeastApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ai/realestate/intellirealeast/IntellirealeastApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ai.realestate.intellirealeast;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntellirealeastApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
